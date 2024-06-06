2225584448311a5242f4759638709f444d0b6475
javohirhalimov <88170054+javohirhalimov@users.noreply.github.com>
2024-06-05 20:02:50 -0400

    Working on my Investment Calculator Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
+
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+       <Header/>
+   <UserInput />
+    </>
+
   )
 }
 
